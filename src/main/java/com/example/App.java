c98e59afe39f6248e9e4df22b0363610110c3140
Tine-m <TM@ek.dk>
2025-09-08 19:04:52 +0200

    change code example

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,4 +1,5 @@
 package com.example;
+import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) {
@@ -8,4 +9,9 @@ public class App {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public boolean isYes(String s) {
+        return s == "yes";  // BUG: use "yes".equals(s)
+    }
+
 }
